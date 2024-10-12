369962aa29d002a35a04e240b1bf9808eeaa294b
fiveseven-lambda <fiveseven.lambda@gmail.com>
2024-10-12 20:19:21 +0900

    表示

--- index.js
+++ index.js
@@ -1,27 +1,10 @@
 import $ from 'jquery'
 import Text from './text.md'
 
-const indices = new URLSearchParams(location.search).get('i').split(',')
+const indices = new URLSearchParams(location.search).get('i').split('.')
 let slug = ''
 
-for (let i = 0; i < 3; i++) {
-  $('#main').append($('<div>'))
-}
 for (const index of indices) {
   slug = Text[slug].c[index]
-  $('#main').append($('<div>'))
+  $('#main').append($('<div>').html('<h2>' + slug + '</h2>' + Text[slug].h))
 }
-
-const generations = $('#main').children()
-const num_generations = generations.length
-generations.each((index, element) => {
-  switch(num_generations - index) {
-    case 1:
-    case 3:
-      $(element).width('15vw')
-      break
-    case 2:
-      $(element).width('70vw')
-      $(element).html(Text[slug].h)
-  }
-});
